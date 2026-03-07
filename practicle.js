ea26a941d7c38e6bfb08aa8f50647299cc65960d
HARSH THAKUR <kspqa126@gmail.com>
2026-03-07 15:19:28 +0530

    Create practicle.js

---
+++ practicle.js
@@ -0,0 +1,44 @@
+<script>
+
+const canvas=document.getElementById("starfield");
+const ctx=canvas.getContext("2d");
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+let stars=[];
+
+for(let i=0;i<250;i++){
+stars.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*2
+});
+}
+
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+stars.forEach(star=>{
+
+star.y+=0.3;
+
+if(star.y>canvas.height){
+star.y=0;
+}
+
+ctx.fillStyle="#38bdf8";
+ctx.fillRect(star.x,star.y,star.size,star.size);
+
+});
+
+requestAnimationFrame(animate);
+
+}
+
+animate();
+
+</script>
+
+</body>
