1531e6c0bae729724322e9897d50efe9099b2e96
HARSH THAKUR <kspqa126@gmail.com>
2026-03-08 02:04:20 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
-apiKey: "AIzaSyA9ses0NXK4OI9JcEH3ym8nDS-kvBlBT_8",
-authDomain: "designtechvlsi.firebaseapp.com",
-projectId: "designtechvlsi",
-storageBucket: "designtechvlsi.appspot.com",
-messagingSenderId: "268545364402",
-appId: "1:268545364402:web:57a3d1d3fb8535998754ab"
+  apiKey: "AIzaSyA9ses0NXK4OI9JcEH3ym8nDS-kvBlBT_8",
+  authDomain: "designtechvlsi.firebaseapp.com",
+  projectId: "designtechvlsi",
+  storageBucket: "designtechvlsi.firebasestorage.app",
+  messagingSenderId: "268545364402",
+  appId: "1:268545364402:web:57a3d1d3fb8535998754ab",
+  measurementId: "G-F5KGZK2JDK"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-const db = firebase.firestore();
+export { auth };
