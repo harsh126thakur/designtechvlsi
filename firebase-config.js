d89cfd322bacf6d8c2729b46a4ec741ae9c818e8
HARSH THAKUR <kspqa126@gmail.com>
2026-03-07 22:35:53 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,19 @@
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "designtechvlsi.firebaseapp.com",
+
+projectId: "designtechvlsi",
+
+storageBucket: "designtechvlsi.appspot.com",
+
+messagingSenderId: "XXXX",
+
+appId: "XXXX"
+
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore();
